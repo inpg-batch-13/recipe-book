efc0e1b5c1f2904d93aa28927db6ee9ac19d1633
Rahmat Hidayat <rahmatramahidayat@gmail.com>
2019-06-26 19:28:35 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var recipes = [{
+  name: 'satay',
+  ingredients: ['chicken', 'soy sauce', 'onion']
+}, {
+  name: 'bakso',
+  ingredients: ['meat', 'water', 'salt']
+}, {
+  name: 'fried rice',
+  ingredients: ['rice', 'ketchup', 'oil', 'egg']
+}]
+
+function findRecipe(menuName) {
+  // your code here
+}
+
+// test case:
+
+console.log(findRecipe('bakso'))
+// MENU NAME: bakso
+// INGREDIENTS:
+// - meat
+// - water
+// - salt
