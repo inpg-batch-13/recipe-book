1be7702c4720906b358f6e2d4814c86c5b269a1a
MuhammadFahmiRidwan <mfahmiridwan50@gmail.com>
2019-06-26 20:53:54 +0700

    done

--- script.js
+++ script.js
@@ -9,8 +9,35 @@ var recipes = [{
   ingredients: ['rice', 'ketchup', 'oil', 'egg']
 }]
 
+
+// function findRecipe(menuName) {
+//   // your code here
+//   var result = []
+//   for(var i = 0; i <= recipes.length; i++) {
+//     if (recipes[i].name === menuName) {
+//     return 'Menu Name :' + recipes[i].name + '\n' +
+//     'INGREDIENTS :' + recipes[i].ingredients 
+//     }
+
+//   }
+//   return result
+// }
+
 function findRecipe(menuName) {
   // your code here
+  var recipes;
+  for (var i = 0; i < recipes.length; i++) {
+    if (recipes[i].name === menuName) {
+      recipe = recipes[i]
+    }
+  }
+    return `
+    Menu Name : ${recipe.name}
+    INGREDIENTS : 
+    ${recipe.ingredients.map(function(str) { return '-' + str}
+    .join('\n'))}
+    ` 
+    
 }
 
 // test case:
